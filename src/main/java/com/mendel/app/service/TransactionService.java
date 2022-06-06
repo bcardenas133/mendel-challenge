1e3d0a48b90f4dadc7924c60461bd8fca560b8ff
bcardenas133 <73718353+bcardenas133@users.noreply.github.com>
2022-06-06 15:08:19 -0300

    adding documentation

--- src/main/java/com/mendel/app/service/TransactionService.java
+++ src/main/java/com/mendel/app/service/TransactionService.java
@@ -8,9 +8,24 @@ import java.util.List;
 
 public interface TransactionService {
 
+    /**
+     * Save transaction in database
+     * @param transactionDTO information to save
+     * @return transaction result
+     */
     SaveTransactionResponseDTO saveTransaction(TransactionDTO transactionDTO);
 
+    /**
+     * Get list of transactionsId related with transaction type
+     * @param transactionType type of transaction to look for
+     * @return list of transactionsId associated to the transaction type
+     */
     List<Long> getTransactionsIdByType(String transactionType);
 
+    /**
+     * Get sum of amounts related with transactionId and connected parentIds
+     * @param transactionId transactionId to look in db
+     * @return total amount 
+     */
     TransactionsSumDTO getTransactionsSum(String transactionId);
 }
